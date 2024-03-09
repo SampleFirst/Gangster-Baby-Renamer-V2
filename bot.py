0c4fb854fcb31baa2ce0e2d7ecf2367c4177b6be
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-09 20:47:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,21 @@
 import logging
-from pyrogram import Client
-import asyncio
-import os
+import logging.config
+
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
 
 TOKEN = os.environ.get("TOKEN", "6483924561:AAGgvHt6ocAShXHah-FQV6IXJ7cxps9WDrA")
 API_ID = int(os.environ.get("API_ID", "10956858"))
 API_HASH = os.environ.get("API_HASH", "cceefd3382b44d4d85be2d83201102b7")
 STRING = os.environ.get("STRING", "BQCnMDoATgm6EV5D0XezMULX5ROFq3EMWEf50Sf3AXg8Dhp1s7VoLwDwqNQYIXgj3xtZUmEw7CNh5VFY0h_vBbPuZsMtIuIkLtPIWiGwdvHOJTwhJY-QrEq2297_yz1WvA6HiTvkgwvJtgfqnYBMwf5-rAMjLp6rfG8czJENj0HjRp1wCMpIRrVMyRWw5HElSGzolRUj-LAm3KO38zKa2_xxbovIRaRX5UMSPOJ9HJHphXJ5WZHuWIbfEH1Zufb29-bj0FNwo_2cOSAJWfkhMOJKElujJmXepFsRYBK01JEKKUV-aFWqBwEv-H_pyRpWlS8Mjm897KZhZoE5T5FuATHQ6GoSYwAAAAGCeNZRAQ")
 SESSION = os.environ.get("SESSION", "rename")
+PORT = environ.get("PORT", "8080")
 
-logging.getLogger().setLevel(logging.INFO)
+from aiohttp import web
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from plugins import web_server
 
 
 class Bot(Client):
@@ -27,11 +33,15 @@ class Bot(Client):
         )
         
     async def start(self):
-         await super().start()
-         me = await self.get_me()
-         self.username = me.username
-         logging.info(f"{me.first_name} with for Pyrogram started on {me.username}.")
-       
+        await super().start()
+        me = await self.get_me()
+        self.username = me.username
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+                                
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
         await super().stop()
@@ -40,6 +50,3 @@ class Bot(Client):
 
 app = Bot()
 app.run()
-
-
-
