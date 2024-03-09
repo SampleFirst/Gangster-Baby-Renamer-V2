b9e37b5834aca4ec7d9e3681db15519d063f2884
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-09 20:18:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ TOKEN = os.environ.get("TOKEN", "6483924561:AAGgvHt6ocAShXHah-FQV6IXJ7cxps9WDrA"
 API_ID = int(os.environ.get("API_ID", "10956858"))
 API_HASH = os.environ.get("API_HASH", "cceefd3382b44d4d85be2d83201102b7")
 STRING = os.environ.get("STRING", "BQCnMDoATgm6EV5D0XezMULX5ROFq3EMWEf50Sf3AXg8Dhp1s7VoLwDwqNQYIXgj3xtZUmEw7CNh5VFY0h_vBbPuZsMtIuIkLtPIWiGwdvHOJTwhJY-QrEq2297_yz1WvA6HiTvkgwvJtgfqnYBMwf5-rAMjLp6rfG8czJENj0HjRp1wCMpIRrVMyRWw5HElSGzolRUj-LAm3KO38zKa2_xxbovIRaRX5UMSPOJ9HJHphXJ5WZHuWIbfEH1Zufb29-bj0FNwo_2cOSAJWfkhMOJKElujJmXepFsRYBK01JEKKUV-aFWqBwEv-H_pyRpWlS8Mjm897KZhZoE5T5FuATHQ6GoSYwAAAAGCeNZRAQ")
+SESSION = os.environ.get("SESSION", "rename")
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -25,13 +26,11 @@ class Bot(Client):
             sleep_threshold=10,
         )
         
-    
-
     async def start(self):
          await super().start()
          me = await self.get_me()
          self.username = me.username
-         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+         logging.info(f"{me.first_name} with for Pyrogram started on {me.username}.")
        
 
     async def stop(self, *args):
@@ -41,3 +40,6 @@ class Bot(Client):
 
 app = Bot()
 app.run()
+
+
+
